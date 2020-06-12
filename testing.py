1c8f2c3e8ef845ab9ec6b14ade92c5baea9169ae
Isabel Gan <isabelggan@gmail.com>
2020-06-12 00:18:43 -0400

    added testing functionality

---
+++ testing.py
@@ -0,0 +1,15 @@
+import markdown_quant as md_quant 
+
+nb_ids = [165313, 219322, 333748, 439119, 464706, 
+            466289, 492523, 531968, 578489, 602217,
+            681455, 884271, 958849, 972366, 972721,
+            1019165, 1047986, 1051197, 1081631, 1124656]
+
+# prints out the results of running a function on all notebooks
+def test_func(func):
+    for nb_id in nb_ids:
+        print(str(nb_id) + " : " + str(func(nb_id)))
+
+# PUT TESTS HERE
+test_func(md_quant.markdown_prop)
+    
