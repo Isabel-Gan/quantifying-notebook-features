2fa335d531670aa9c99c462e4c0734c9d5d10d1d
Isabel Gan <isabelggan@gmail.com>
2020-10-30 02:23:27 -0400

    no duplicate repositories - clustering

--- full-output/full-kproto-clustering.r
+++ full-output/full-kproto-clustering.r
@@ -36,20 +36,39 @@ library("clustMixType")
 
 
 # load in the data
-combined_original <- read.csv('csv-files/full_groups_combined.csv')
+# combined_original <- read.csv('csv-files/full_groups_combined.csv')
 
 # drop the first three columns
-combined_df <- combined_original[ , !(names(combined_original) %in% c("X", "nb_id", "repo_id"))]
+# combined_df <- combined_original[ , !(names(combined_original) %in% c("X", "nb_id", "repo_id"))]
 
 # kproto clustering for combined
+# Es <- numeric(10)
+# for (i in 1:10) {
+#     kpres <- kproto(combined_df, k = i, nstart = 10)
+#     Es[i] <- kpres$tot.withinss
+# }
+
+# create plot and save to file
+# jpeg("groups_combined_clusters.jpg", width = 350, height = 350)
+# plot(1:10, Es, type = "b", ylab = "Objective Function", xlab = "# Clusters",
+#     main = "Combined - Scree Plot")
+# dev.off()
+
+# load in the data
+no_dup_original <- read.csv('csv-files/full_no_repo_dups.csv')
+
+# drop the first three columns
+no_dup_df <- no_dup_original[ , !(names(no_dup_original) %in% c("X", "nb_id", "repo_id"))]
+
+# kproto clustering for no repo dups
 Es <- numeric(10)
 for (i in 1:10) {
-    kpres <- kproto(combined_df, k = i, nstart = 10)
+    kpres <- kproto(no_dup_df, k = i, nstart = 10)
     Es[i] <- kpres$tot.withinss
 }
 
 # create plot and save to file
-jpeg("groups_combined_clusters.jpg", width = 350, height = 350)
+jpeg("no_repo_dups_clusters.jpg", width = 350, height = 350)
 plot(1:10, Es, type = "b", ylab = "Objective Function", xlab = "# Clusters",
-    main = "Combined - Scree Plot")
+    main = "No Repository Duplicates - Scree Plot")
 dev.off()
