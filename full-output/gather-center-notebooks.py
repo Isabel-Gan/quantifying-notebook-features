3ac49b5ab1ae01b7a823e8054ba99831e37fa9d7
Isabel Gan <isabelggan@gmail.com>
2020-12-07 03:48:38 -0500

    gathered top notebooks by cluster

--- full-output/gather-center-notebooks.py
+++ full-output/gather-center-notebooks.py
@@ -3,40 +3,40 @@ import csv
 import numpy as np
 
 # import kprotos ouptut
-outliers_df = pd.read_pickle('full_reduced_outliers.pkl')
-dists_df = pd.read_feather('prototypes-output/outliers_dists4.feather')
+data_df = pd.read_pickle('full_log_normalized.pkl')
+dists_df = pd.read_feather('prototypes-output/norm_dists4.feather')
 dists_df = dists_df.transpose()
-clusters_df = pd.read_feather('prototypes-output/outliers_clusters4.feather')
+clusters_df = pd.read_feather('prototypes-output/norm_clusters4.feather')
 
 # modify df to include cluster
-outliers_df['cluster'] = clusters_df[['outliers_kprotos$cluster']].to_numpy()
-outliers_df['cluster_dist'] = outliers_df['cluster']
+data_df['cluster'] = clusters_df[['norm_kprotos$cluster']].to_numpy()
+data_df['cluster_dist'] = data_df['cluster']
 
 # add distances (to all centers)
-outliers_df[0] = dists_df[[0]].to_numpy()
-outliers_df[1] = dists_df[[1]].to_numpy()
-outliers_df[2] = dists_df[[2]].to_numpy()
-outliers_df[3] = dists_df[[3]].to_numpy()
+data_df[0] = dists_df[[0]].to_numpy()
+data_df[1] = dists_df[[1]].to_numpy()
+data_df[2] = dists_df[[2]].to_numpy()
+data_df[3] = dists_df[[3]].to_numpy()
 
 # center to specific cluster
-outliers_df['cluster_dist'] = outliers_df['cluster_dist'].apply(lambda c : outliers_df[c - 1])
+data_df['cluster_dist'] = data_df['cluster_dist'].apply(lambda c : data_df[c - 1])
 
 # drop the distance columns
-outliers_df = outliers_df.drop([0, 1, 2, 3], axis = 1)
+data_df = data_df.drop([0, 1, 2, 3], axis = 1)
 
 # output to pickle and csv
-outliers_df.to_pickle('outliers_clusters.pkl')
-outliers_df.to_csv('csv-files/outliers_clusters.csv')
+data_df.to_pickle('norm_clusters.pkl')
+data_df.to_csv('csv-files/norm_clusters.csv')
 
 # import data file
 notebooks_df = pd.read_pickle('../full-dataset/notebooks.pkl')
 
 # get the top notebooks
-with open('top_notebooks.txt', 'w') as outfile:
+with open('top_notebooks_norm.txt', 'w') as outfile:
 
     for cluster in range(1, 5):
 
-        cluster_df = outliers_df[outliers_df['cluster'] == cluster]
+        cluster_df = data_df[data_df['cluster'] == cluster]
         closest_notebooks = cluster_df.nsmallest(20, ['cluster_dist'])['nb_id'].to_list()
 
         closest_notebooks_df = notebooks_df[notebooks_df['nb_id'].isin(closest_notebooks)]
